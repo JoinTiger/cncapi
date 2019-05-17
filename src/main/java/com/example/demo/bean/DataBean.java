5dc2953302f8bed8744d2b63183d2ea136fa4e76
luyiwei <1248153626@qq.com>
2019-05-17 15:05:57 +0800

    201905171505

--- src/main/java/com/example/demo/bean/DataBean.java
+++ src/main/java/com/example/demo/bean/DataBean.java
@@ -39,6 +39,130 @@ public class DataBean implements Serializable {
     private List<ElecDataList> elecDataList = new ArrayList<>();
 
 
+    public DataBean() {
+    }
+
+
+    public DataBean(Long uploadTime, Integer version, Integer macAxNum, String macModel, String macNO, String macSN, String ipcCode, String contractCode, String customerName, String loginName, NcVer ncVer, List<Servo> servos, List<ElecDataList> elecDataList) {
+        this.uploadTime = uploadTime;
+        this.version = version;
+        this.macAxNum = macAxNum;
+        this.macModel = macModel;
+        this.macNO = macNO;
+        this.macSN = macSN;
+        this.ipcCode = ipcCode;
+        this.contractCode = contractCode;
+        this.customerName = customerName;
+        this.loginName = loginName;
+        this.ncVer = ncVer;
+        this.servos = servos;
+        this.elecDataList = elecDataList;
+    }
+
+
+    public Long getUploadTime() {
+        return uploadTime;
+    }
+
+    public void setUploadTime(Long uploadTime) {
+        this.uploadTime = uploadTime;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public Integer getMacAxNum() {
+        return macAxNum;
+    }
+
+    public void setMacAxNum(Integer macAxNum) {
+        this.macAxNum = macAxNum;
+    }
+
+    public String getMacModel() {
+        return macModel;
+    }
+
+    public void setMacModel(String macModel) {
+        this.macModel = macModel;
+    }
+
+    public String getMacNO() {
+        return macNO;
+    }
+
+    public void setMacNO(String macNO) {
+        this.macNO = macNO;
+    }
+
+    public String getMacSN() {
+        return macSN;
+    }
+
+    public void setMacSN(String macSN) {
+        this.macSN = macSN;
+    }
+
+    public String getIpcCode() {
+        return ipcCode;
+    }
+
+    public void setIpcCode(String ipcCode) {
+        this.ipcCode = ipcCode;
+    }
+
+    public String getContractCode() {
+        return contractCode;
+    }
+
+    public void setContractCode(String contractCode) {
+        this.contractCode = contractCode;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public NcVer getNcVer() {
+        return ncVer;
+    }
+
+    public void setNcVer(NcVer ncVer) {
+        this.ncVer = ncVer;
+    }
+
+    public List<Servo> getServos() {
+        return servos;
+    }
+
+    public void setServos(List<Servo> servos) {
+        this.servos = servos;
+    }
+
+    public List<ElecDataList> getElecDataList() {
+        return elecDataList;
+    }
+
+    public void setElecDataList(List<ElecDataList> elecDataList) {
+        this.elecDataList = elecDataList;
+    }
 }
 
 
